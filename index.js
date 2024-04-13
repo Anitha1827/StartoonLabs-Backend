826ee66f42db56d7a568c8873ee02e07ca58c33e
Anitha1827 <anitha.h2711@gmail.com>
2024-04-13 08:45:26 +0530

    cors

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ dotenv.config();
 //middlewares
 let app = express();
 app.use(cors());
+const corsOptions = {
+    origin: 'https://startoonlab-app.netlify.app',
+  };
 app.use(express.json());
 let PORT = process.env.PORT || 8000;
 
